fdcd7a8048b8dae007285902a56322d63f36654c
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2023-12-31 19:42:03 +0530

    Create Maths.java

---
+++ Add, Sub, Mul, Div/Maths.java
@@ -0,0 +1,14 @@
+public class Maths {
+
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+
+
+        System.out.println(num1 + num1);     //Add
+        System.out.println(num1 - num2);     //substract
+        System.out.println(num1 * num2);     //multiply
+        System.out.println(num1 / num2);     //division
+    }
+
+}
