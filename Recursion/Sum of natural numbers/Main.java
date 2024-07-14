db4c056c90dcd527bae825f767b1eb88d6418821
Ruturaj Jadhav <109014091+ruturajjadhav07@users.noreply.github.com>
2024-07-14 14:22:32 +0530

    Update Main.java

--- Recursion/Sum of natural numbers/Main.java
+++ Recursion/Sum of natural numbers/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+
+    public static void printNum(int i, int num, int sum) {
+        if (i == num) {
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printNum(i+1, num, sum);
+    }
+
+    public static void main(String[] args) {
+        printNum(1, 5, 0);
+    }
+}
 
